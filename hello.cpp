de89582d3664390b1d79dcc618a45a24ec1bbadc
Huy <giahuyjae07082001@gmail.com>
2025-11-18 21:42:56 +0700

    first-commit

---
+++ hello.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main() {
+    
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    vector<int> a(5);
+    for (int i = 0; i < 5; ++i) {
+        cin >> a[i];
+    }
+    for (int i = 4; i >= 0; --i) {
+        cout << a[i] << " ";
+    }
+    return 0;
+}
